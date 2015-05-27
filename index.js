c01911181bf898b9e66104ec410481219345c05f
Luke Melia <luke@lukemelia.com>
2015-05-27 18:12:09 -0400

    Expect a promisy redis client (like then-redis) to be passed

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var async     = require('async');
-var Promise   = require('bluebird');
+var Bluebird   = require('bluebird');
 var _defaults = require('lodash/object/defaults');
 
 var EmberCliDeployError = require('./errors/ember-cli-deploy-error');
@@ -13,7 +13,7 @@ var _getOpts = function (opts) {
   return _defaults({}, opts, _defaultOpts);
 };
 
-var fetchIndex = function (appName, req, client, opts) {
+var fetchIndex = function (appName, req, redisClient, opts) {
   opts = _getOpts(opts);
 
   var indexkey;
@@ -21,47 +21,32 @@ var fetchIndex = function (appName, req, client, opts) {
     var queryKey = req.query[opts.revisionQueryParam].replace(/[^A-Za-z0-9]/g, '');
     indexkey = appName + ':' + queryKey;
   }
+  var customIndexKeyWasSpecified = !!indexkey;
 
-  return new Promise(function(resolve, reject) {
-    async.waterfall([
-      // 1. Get the current indexkey, or use the one provided in the query param
-      function(callback) {
-        if (indexkey) {
-          callback(null, indexkey, false);
-        } else {
-          client.get(appName + ":current", function(err, indexkey) {
-            if (err || !indexkey) {
-              callback(new EmberCliDeployError("There's no " + appName + ":current revision. The site is down.", true));
-            } else {
-              callback(null, indexkey, true);
-            }
-          });
-        }
-      },
-      // 2. Get the index page out of redis
-      function(indexkey, isCurrent, callback) {
-        client.get(indexkey, function(err, index) {
-          if (err || !index) {
-            if (isCurrent) {
-              callback(new EmberCliDeployError("There's no " + indexkey + " revision. The site is down", true));
-            } else {
-              callback(new EmberCliDeployError("There's no " + indexkey + " revision.", false));
-            }
-          } else {
-            callback(null, index);
-          }
-        });
-      },
-      function(index, callback) {
-        callback(null, index);
-      }
-    ], function(err, indexHtml) {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(indexHtml);
-      }
-    });
+  function retrieveIndexKey(){
+    if (indexkey) {
+      return Bluebird.resolve(indexkey);
+    } else {
+      return redisClient.get(appName + ":current").then(function(result){
+        if (!result) { throw new Error(); }
+        return result;
+      }).catch(function(){
+        throw new EmberCliDeployError("There's no " + appName + ":current revision. The site is down.", true);
+      });
+    }
+  };
+
+  return retrieveIndexKey().then(function(indexkey){
+    return redisClient.get(indexkey);
+  }).then(function(indexHtml){
+      if (!indexHtml) { throw new Error(); }
+      return indexHtml;
+  }).catch(function(err){
+    if (err.name === 'EmberCliDeployError') {
+      throw err;
+    } else {
+      throw new EmberCliDeployError("There's no " + indexkey + " revision. The site is down.", !customIndexKeyWasSpecified);
+    }
   });
 };
 
--- test/index-test.js
+++ test/index-test.js
@@ -1,6 +1,7 @@
 var expect = require('chai').expect;
 var sinon =  require('sinon');
 var rewire = require('rewire');
+var Bluebird = require('bluebird');
 
 var subject = rewire('../index');
 
@@ -29,23 +30,33 @@ describe('fetchIndex', function() {
 
   before(function() {
     sandbox = sinon.sandbox.create();
-    redis = require('redis-mock').createClient();
+    redis = {
+      _storage: {},
+      get: function(key){
+        return Bluebird.resolve(this._storage[key]);
+      },
+      set: function(key, value){
+        this._storage[key] = value;
+        return Bluebird.resolve(value);
+      },
+      del: function(key){
+        delete this._storage[key];
+        return Bluebird.resolve();
+      },
+      flushall: function(){
+        this._storage = {};
+        return Bluebird.resolve();
+      }
+    }
   });
 
   beforeEach(function() {
-    redisApi = {
-      get: function() {}
-    };
     redisSpy = sandbox.spy(redis, 'get');
   });
 
   afterEach(function() {
     sandbox.restore();
     redis.flushall();
-  });
-
-  after(function() {
-    redis.end();
   });
 
   it('normalizes spaces in revisionQueryParam', function(done) {
@@ -55,15 +66,16 @@ describe('fetchIndex', function() {
       }
     };
 
-    redis.set('myapp:abc123', 'foo');
-
-    subject.fetchIndex('myapp', req, redis).then(function() {
-      expect(redisSpy.calledWith('myapp:abc123')).to.be.true;
-      expect(redisSpy.calledWith('myapp:abc 123')).to.be.false;
-      done();
-    }).catch(function(err) {
-      done(err);
+    redis.set('myapp:abc123', 'foo').then(function(){
+      subject.fetchIndex('myapp', req, redis).then(function() {
+        expect(redisSpy.calledWith('myapp:abc123')).to.be.true;
+        expect(redisSpy.calledWith('myapp:abc 123')).to.be.false;
+        done();
+      }).catch(function(err) {
+        done(err);
+      });
     });
+
   });
 
   it('removes special chars revisionQueryParam', function(done) {
@@ -73,96 +85,100 @@ describe('fetchIndex', function() {
       }
     };
 
-    redis.set('myapp:abc123', 'foo');
-
-    subject.fetchIndex('myapp', req, redis).then(function() {
-      expect(redisSpy.calledWith('myapp:abc123')).to.be.true;
-      expect(redisSpy.calledWith('myapp:ab@*#!c(@)123')).to.be.false;
-      done();
-    }).catch(function(err) {
-      done(err);
+    redis.set('myapp:abc123', 'foo').then(function(){
+      subject.fetchIndex('myapp', req, redis).then(function() {
+        expect(redisSpy.calledWith('myapp:abc123')).to.be.true;
+        expect(redisSpy.calledWith('myapp:ab@*#!c(@)123')).to.be.false;
+        done();
+      }).catch(function(err) {
+        done(err);
+      });
     });
   });
 
   it('fails the promise with a critical error if appName:current is not present', function(done) {
-    redis.del('myapp:current');
-
-    subject.fetchIndex('myapp', basicReq, redis).then(function() {
-      done("Promise should not have resolved.");
-    }).catch(function(err) {
-      expect(redisSpy.calledWith('myapp:current')).to.be.true;
-      expect(err.critical).to.be.true;
-      done();
+    redis.del('myapp:current').then(function(){
+      subject.fetchIndex('myapp', basicReq, redis).then(function() {
+        done("Promise should not have resolved.");
+      }).catch(function(err) {
+        expect(redisSpy.calledWith('myapp:current')).to.be.true;
+        expect(err.critical).to.be.true;
+        done();
+      });
     });
   });
 
   it('fails the promise with a critical error if revision pointed to by appName:current is not present', function(done) {
-    redis.set('myapp:current', 'myapp:abc123');
-    redis.del('myapp:abc123');
-
-    subject.fetchIndex('myapp', basicReq, redis).then(function() {
-      done("Promise should not have resolved.");
-    }).catch(function(err) {
-      expect(redisSpy.calledWith('myapp:current')).to.be.true;
-      expect(redisSpy.calledWith('myapp:abc123')).to.be.true;
-      expect(err.critical).to.be.true;
-      done();
+    redis.set('myapp:current', 'myapp:abc123').then(function(){
+      return redis.del('myapp:abc123');
+    }).then(function(){
+      subject.fetchIndex('myapp', basicReq, redis).then(function() {
+        done("Promise should not have resolved.");
+      }).catch(function(err) {
+        expect(redisSpy.calledWith('myapp:current')).to.be.true;
+        expect(redisSpy.calledWith('myapp:abc123')).to.be.true;
+        expect(err.critical).to.be.true;
+        done();
+      });
     });
   });
 
   it('fails the promise with a non-critical error if revision requestd by query param is not present', function(done) {
-    redis.del('myapp:abc123');
     req = {
       query: {
         index_key: 'abc123'
       }
     };
-
-    subject.fetchIndex('myapp', req, redis).then(function() {
-      done("Promise should not have resolved.");
-    }).catch(function(err) {
-      expect(redisSpy.calledWith('myapp:abc123')).to.be.true;
-      expect(err.critical).to.be.false;
-      done();
+    redis.del('myapp:abc123').then(function(){
+      subject.fetchIndex('myapp', req, redis).then(function() {
+        done("Promise should not have resolved.");
+      }).catch(function(err) {
+        expect(redisSpy.calledWith('myapp:abc123')).to.be.true;
+        expect(err.critical).to.be.false;
+        done();
+      });
     });
   });
 
   it('resolves the promise with the index html requested', function(done) {
     var currentHtmlString = '<html><body>1</body></html>';
-    redis.set('myapp:current', 'myapp:abc123');
-    redis.set('myapp:abc123', currentHtmlString);
-
-    subject.fetchIndex('myapp', basicReq, redis).then(function(html) {
-      expect(redisSpy.calledWith('myapp:current')).to.be.true;
-      expect(redisSpy.calledWith('myapp:abc123')).to.be.true;
-      expect(html).to.equal(currentHtmlString);
-      done();
-    }).catch(function(err) {
-      done("Promise should not have failed.");
-    });
+    Bluebird.all([
+      redis.set('myapp:current', 'myapp:abc123'),
+      redis.set('myapp:abc123', currentHtmlString),
+    ]).then(function(){
+      subject.fetchIndex('myapp', basicReq, redis).then(function(html) {
+        expect(redisSpy.calledWith('myapp:current')).to.be.true;
+        expect(redisSpy.calledWith('myapp:abc123')).to.be.true;
+        expect(html).to.equal(currentHtmlString);
+        done();
+      }).catch(function(err) {
+        done("Promise should not have failed.");
+      });
+    })
   });
 
   it('resolves the promise with the index html requested (specific revision)', function(done) {
     var currentHtmlString = '<html><body>1</body></html>';
     var newDeployHtmlString = '<html><body>2</body></html>';
-    redis.set('myapp:current', 'myapp:abc123');
-    redis.set('myapp:abc123', currentHtmlString);
-    redis.set('myapp:def456', newDeployHtmlString);
-
     var req = {
       query: {
         index_key: 'def456'
       }
     };
-
-    subject.fetchIndex('myapp', req, redis).then(function(html) {
-      expect(redisSpy.calledWith('myapp:current')).to.be.false;
-      expect(redisSpy.calledWith('myapp:abc123')).to.be.false;
-      expect(redisSpy.calledWith('myapp:def456')).to.be.true;
-      expect(html).to.equal(newDeployHtmlString);
-      done();
-    }).catch(function(err) {
-      done("Promise should not have failed.");
+    Bluebird.all([
+      redis.set('myapp:current', 'myapp:abc123'),
+      redis.set('myapp:abc123', currentHtmlString),
+      redis.set('myapp:def456', newDeployHtmlString)
+    ]).then(function(){
+      subject.fetchIndex('myapp', req, redis).then(function(html) {
+        expect(redisSpy.calledWith('myapp:current')).to.be.false;
+        expect(redisSpy.calledWith('myapp:abc123')).to.be.false;
+        expect(redisSpy.calledWith('myapp:def456')).to.be.true;
+        expect(html).to.equal(newDeployHtmlString);
+        done();
+      }).catch(function(err) {
+        done("Promise should not have failed.");
+      });
     });
   });
 });
